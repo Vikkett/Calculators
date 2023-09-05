fd2fd198579de0061669350993119f5168c9f68f
Vikkett <144105889+Vikkett@users.noreply.github.com>
2023-09-05 16:28:00 +0200

    05.09.2023

--- Calculators/Program.cs
+++ Calculators/Program.cs
@@ -10,10 +10,11 @@ namespace Calculators
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculatrcie");
+            /*Console.WriteLine("Calculatrcie");
 
             Console.WriteLine();
             Console.WriteLine("Write the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
             string firstNum = Console.ReadLine();
 
             Console.WriteLine();
@@ -22,6 +23,7 @@ namespace Calculators
 
             Console.WriteLine();
             Console.WriteLine("Write the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
             string secondNum = Console.ReadLine();
 
             string operations = Console.ReadLine();
@@ -30,6 +32,7 @@ namespace Calculators
             {
                 case 1:
                     Console.WriteLine("+");
+                    
                     break;
                 case 2:
                     Console.WriteLine("-");
@@ -39,6 +42,47 @@ namespace Calculators
                     break;
                 case 4:
                     Console.WriteLine("/");
+                    break;
+
+
+            }*/
+
+
+
+
+
+
+            Console.WriteLine("Calculatrcie");
+
+            Console.WriteLine();
+            Console.WriteLine("Write the first number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Choose the operators");
+            string operators = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Write the second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            string operations = Console.ReadLine();
+           
+            switch (operations)
+            {
+                case "+":
+                    
+                    Console.WriteLine("");
+
+                    break;
+                case "-":
+                    Console.WriteLine("");
+                    break;
+                case "/":
+                    Console.WriteLine("");
+                    break;
+                case "*":
+                    Console.WriteLine("");
                     break;
 
 
